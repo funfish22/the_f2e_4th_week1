0b1e5d81f44c52cbd76c384218ac122f7c97cd19
t_aga <aga.lhj@link8tech.tw>
2022-11-18 18:02:34 +0800

    調整主題區塊動畫

--- src/components/theme/index.jsx
+++ src/components/theme/index.jsx
@@ -1,3 +1,6 @@
+import { useRef, useLayoutEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./style.scss";
 import Introduce from "./child/introduce";
 import forwardHouse from "@/assets/images/theme/forward-house.png";
@@ -9,8 +12,29 @@ import cloud2 from "@/assets/images/theme/cloud2.png";
 import introduceData from "./child/introduce/data";
 
 const Theme = () => {
+    gsap.registerPlugin(ScrollTrigger);
+    const el = useRef();
+    const q = gsap.utils.selector(el);
+    const tl = useRef();
+
+    useLayoutEffect(() => {
+        let speed = 100;
+        tl.current = gsap
+            .timeline({
+                scrollTrigger: {
+                    trigger: ".theme .container",
+                    start: "top 60%",
+                    end: "+=100",
+                    scrub: 5
+                },
+                ease: "strong.inOut",
+            })
+            .to(q(".house--back"), { y: 0, scale: 1, ease: "power1.in" }, 0)
+            .to(q(".house--forward"), { y: 0, scale: 1, ease: "power1.in" }, 0)
+    })
+
     return (
-        <section className="theme">
+        <section className="theme" ref={el}>
             <div className="container">
                 <h1 className="title">
                     年度最強合作，<span className="title__big">三大</span>
