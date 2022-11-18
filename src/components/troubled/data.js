64c79aa3e3fa1900a1bcb3f44f7054eb7556199e
t_aga <aga.lhj@link8tech.tw>
2022-11-18 14:56:49 +0800

    調整困擾區塊資料

---
+++ src/components/troubled/data.js
@@ -0,0 +1,20 @@
+import setp1 from "@/assets/images/troubled/step1.png";
+import setp2 from "@/assets/images/troubled/step2.png";
+import setp3 from "@/assets/images/troubled/step3.png";
+
+const troubledData = [
+    {
+        text: "羨慕別人的酷酷網頁動畫嗎?",
+        img: setp1,
+    },
+    {
+        text: "滿足不了同事的許願?",
+        img: setp2,
+    },
+    {
+        text: "動畫技能數太雜無從下手?",
+        img: setp3,
+    },
+];
+
+export default troubledData;
--- src/components/troubled/index.jsx
+++ src/components/troubled/index.jsx
@@ -1,12 +1,10 @@
-import { useEffect, useRef, useLayoutEffect } from "react";
+import { useRef, useLayoutEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./style.scss";
 import circleMaskOne from "@/assets/images/troubled/troubled-mask1.png";
 import circleMaskTwo from "@/assets/images/troubled/troubled-mask2.png";
-import setp1 from "@/assets/images/troubled/step1.png";
-import setp2 from "@/assets/images/troubled/step2.png";
-import setp3 from "@/assets/images/troubled/step3.png";
+import troubledData from "./data";
 
 const Troubled = () => {
     gsap.registerPlugin(ScrollTrigger);
@@ -27,15 +25,15 @@ const Troubled = () => {
             })
             .to(q(".step-list:nth-of-type(1)"), {
                 y: 0,
-                opacity: 1
+                opacity: 1,
             })
             .to(q(".step-list:nth-of-type(2)"), {
                 y: 0,
-                opacity: 1
+                opacity: 1,
             })
             .to(q(".step-list:nth-of-type(3)"), {
                 y: 0,
-                opacity: 1
+                opacity: 1,
             });
     }, []);
 
@@ -54,41 +52,19 @@ const Troubled = () => {
             <div className="container">
                 <h1 className="title">你是否也有以下困擾?</h1>
                 <ul className="step">
-                    <li className="step-list">
-                        <div className="step-list-img">
-                            <img
-                                src={setp1}
-                                alt=""
-                                className="step-list-img__img"
-                            />
-                        </div>
-                        <p className="step-list__info">
-                            羨慕別人的酷酷網頁動畫嗎?
-                        </p>
-                    </li>
-                    <li className="step-list">
-                        <div className="step-list-img">
-                            <img
-                                src={setp2}
-                                alt=""
-                                className="step-list-img__img"
-                            />
-                        </div>
-                        <p className="step-list__info">滿足不了同事的許願?</p>
-                    </li>
-                    <li className="step-list">
-                        <div className="step-list-img">
-                            <img
-                                src={setp3}
-                                alt=""
-                                className="step-list-img__img"
-                            />
-                            <span className="step-list-img__decorate">?</span>
-                        </div>
-                        <p className="step-list__info">
-                            動畫技能數太雜無從下手?
-                        </p>
-                    </li>
+                    {troubledData &&
+                        troubledData.map((item, index) => (
+                            <li className="step-list" key={index}>
+                                <div className="step-list-img">
+                                    <img
+                                        src={item.img}
+                                        alt=""
+                                        className="step-list-img__img"
+                                    />
+                                </div>
+                                <p className="step-list__info">{item.text}</p>
+                            </li>
+                        ))}
                 </ul>
             </div>
         </section>
