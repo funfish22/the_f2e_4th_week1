cd8adfa9616c40193374caae1707bdea514f9bc1
t_aga <aga.lhj@link8tech.tw>
2022-11-18 10:37:31 +0800

    新增頁腳

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,22 @@ import Description from "./components/description";
 import Contest from "./components/contest";
 import Question from "./components/question";
 import Sponsor from "./components/sponsor";
+import Footer from "./components/footer";
 
 const App = () => {
     return (
-        <main>
-            <Banner />
-            <Troubled />
-            <Theme />
-            <Description />
-            <Contest />
-            <Question />
-            <Sponsor />
-        </main>
+        <>
+            <main>
+                <Banner />
+                <Troubled />
+                <Theme />
+                <Description />
+                <Contest />
+                <Question />
+                <Sponsor />
+            </main>
+            <Footer />
+        </>
     );
 };
 
---
+++ src/components/footer/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./style.module.scss";
+
+const Footer = () => {
+    return (
+        <footer className={styles.root}>Copyright © 2022 Hsu Lan Design.All rights reserved.</footer>
+    )
+}
+
+export default Footer;
