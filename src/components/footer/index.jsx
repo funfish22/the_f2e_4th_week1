68e0c4df59cb9c345e92c0dae069583d463c0ef7
t_aga <aga.lhj@link8tech.tw>
2022-11-18 10:58:07 +0800

    新增立即報名按鈕

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -2,7 +2,7 @@ import styles from "./style.module.scss";
 
 const Footer = () => {
     return (
-        <footer className={styles.root}>Copyright © 2022 Hsu Lan Design.All rights reserved.</footer>
+        <footer className={styles.root}>Copyright © 2022 Hsu Lan Design.All rights reserved. <a href="javascript:;" className="registrationBtnNow">立即<br />報名</a></footer>
     )
 }
 
