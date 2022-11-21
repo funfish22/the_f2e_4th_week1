ea60b015620dcab23baa2f3f7c44d277b1ae440f
t_aga <aga.lhj@link8tech.tw>
2022-11-21 14:21:54 +0800

    調整活動說明的資料

---
+++ src/components/description/data.js
@@ -0,0 +1,16 @@
+const descriptionData = [
+    {
+        title: "報名時間",
+        info: "10/13~10/30\n截止前可修改報名組別",
+    },
+    {
+        title: "登錄作品",
+        info: "10/31~11/28 依賽程登錄作品\n10/31(一) UI、團體組開賽\n11/7(一) 前端組開賽",
+    },
+    {
+        title: "額外競賽",
+        info: "主題豐厚獎金等著你",
+    },
+];
+
+export default descriptionData;
--- src/components/description/index.jsx
+++ src/components/description/index.jsx
@@ -1,4 +1,5 @@
 import style from "./style.module.scss";
+import descriptionData from "./data";
 
 const Description = () => {
     return (
@@ -8,21 +9,16 @@ const Description = () => {
                     <h1 className="title">活動說明</h1>
                     <div className="progressBar"></div>
                     <ul className="dec">
-                        <li className="dec-li">
-                            <span className="dec-li__arrow"></span>
-                            <p className="dec-li__title">報名時間</p>
-                            <p className="dec-li__info">10/13~10/30<br />截止前可修改報名組別</p>
-                        </li>
-                        <li className="dec-li">
-                            <span className="dec-li__arrow"></span>
-                            <p className="dec-li__title">登錄作品</p>
-                            <p className="dec-li__info">10/31~11/28 依賽程登錄作品<br />10/31(一) UI、團體組開賽<br />11/7(一) 前端組開賽</p>
-                        </li>
-                        <li className="dec-li">
-                            <span className="dec-li__arrow"></span>
-                            <p className="dec-li__title">額外競賽</p>
-                            <p className="dec-li__info">主題豐厚獎金等著你</p>
-                        </li>
+                        {descriptionData &&
+                            descriptionData.map((item, index) => (
+                                <li className="dec-li" key={index}>
+                                    <span className="dec-li__arrow"></span>
+                                    <p className="dec-li__title">
+                                        {item.title}
+                                    </p>
+                                    <p className="dec-li__info">{item.info}</p>
+                                </li>
+                            ))}
                     </ul>
                 </div>
                 <div className="flashing flashing--left">
